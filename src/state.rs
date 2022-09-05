bcb1c32c3209f50d61e868511e01ea1f47df7ca7
Maxime Richard <maxrichard@apple.com>
2022-09-05 21:20:36 +1000

    Support an execute message where an account can send coins to the contract and specify two accounts that can withdraw the coins (for simplicity, split coins evenly across the two destination accounts)

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,7 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, DepsMut, StdResult, Response, Uint128, Uint64};
+use cw_storage_plus::{Item,Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
@@ -9,3 +9,20 @@ pub struct Config {
 }
 
 pub const CONFIG: Item<Config> = Item::new("config");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Pot {
+    /// target_addr is the address that will receive the pot
+    pub target_addr: Addr,
+    /// collected keeps information on how much is collected for this pot.
+    pub collected: Uint128,
+}
+
+/// POT_SEQ holds the last pot ID
+pub const POTS: Map<&str, Pot> = Map::new("pot");
+
+pub fn save_pot(deps: DepsMut, pot1: &Pot, pot2: &Pot,) -> StdResult<()> {
+    // save pot with id
+    POTS.save(deps.storage, pot1.target_addr.as_str(), pot1);
+    POTS.save(deps.storage, pot2.target_addr.as_str(), pot2)
+}
